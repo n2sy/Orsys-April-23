0f12b7a356afd0d0dae8da79a2ba7776bc5edbda
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-04-13 15:19:14 +0100

    second step

---
+++ src/app/liste/liste.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Candidat } from '../models/candidat';
+
+@Component({
+  selector: 'app-liste',
+  templateUrl: './liste.component.html',
+  styleUrls: ['./liste.component.css'],
+})
+export class ListeComponent {}
---
+++ src/app/models/candidat.ts
@@ -0,0 +1,12 @@
+export class Candidat {
+  constructor(public _id: number, public prenom: string, public nom?: string) {}
+  //   public _id: number;
+  //   public prenom: string;
+  //   public nom: string;
+
+  //   constructor(_id, prenom, nom) {
+  //     this._id = _id;
+  //     this.prenom = prenom;
+  //     this.nom = nom;
+  //   }
+}
